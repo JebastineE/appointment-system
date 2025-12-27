f502afd8945327e7d0df43edf135eb29b8c2895d
Jebastine E <jebastine230906@gmail.com>
2025-12-27 17:23:06 +0530

    Updated appointment logic and UI

--- Backend/server.js
+++ Backend/server.js
@@ -8,28 +8,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Required for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
 app.use(express.json());
 
-// Serve frontend
 app.use(express.static(path.join(__dirname, "../Frontend")));
 
-// Root route -> frontend index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/index.html"));
 });
 
-// API routes
 app.use("/api", appointmentRoutes);
 
 import testRoute from "./routes/test.js";
 app.use("/api", testRoute);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
